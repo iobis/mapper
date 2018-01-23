f38c7ae1b4e41bd4d26820c3c1cc8d6d079e81d8
Pieter Provoost <pieterprovoost@gmail.com>
2018-01-23 16:33:00 +0100

    layers panel styling

--- app/components/LayersComponent.vue
+++ app/components/LayersComponent.vue
@@ -1,35 +1,35 @@
 <template>
 	<div>
 		<span class="sideheader">Layers</span>
-		<div class="sidepanel">
-			<div class="content">
-                <span v-if="sharedState.layers.length == 0" class="no-results">No layers.</span>
-				<ul v-if="sharedState.layers.length > 0" class="suggestions">
-					<li v-for="layer in sharedState.layers" class="layerbox">
-						<div class="closeicon pull-right">
-							<span class="oi oi-x clickable" v-on:click="removeLayer(layer)"></span>
-							<br/><span class="oi oi-grid-three-up clickable" v-on:click="viewData(layer)"></span>
-						</div>
 
-						<span v-for="(taxon, index) in layer.taxa">
-							{{ taxon.scientificName }} <span class="count">{{ taxon.scientificNameAuthorship }}</span><span v-if="index < layer.taxa.length - 1"><br/></span>
-						</span>
+		<div v-if="sharedState.layers.length == 0" class="sidepanel"><div class="content"><p class="no-results">No layers.</p></div></div>
 
-						<span v-if="layer.startyear != null || layer.endyear != null" class="count">
-							<br/>{{ layer.startyear }} - {{ layer.endyear }}
-						</span>
-						<span v-if="layer.geometry != null" class="count">
-							<br/>polygon filter
-						</span>
-						<br/><span class="count">{{ layer.count }} records</span>
-						<br/><div>
-						<span v-if="layer.colors.length > 1" class="colorsquare" :style="{ 'background-color': color }" v-for="color in layer.colors"></span>
-						<span v-if="layer.colors.length == 1" class="colorsquarelong" :style="{ 'background-color': layer.colors[0] }"></span>
-						</div>
-					</li>
-				</ul>
+		<div v-for="layer in sharedState.layers" class="sidelayer">
+
+			<div class="layericon pull-right clickable" v-on:click="removeLayer(layer)">
+                <span class="oi oi-x"></span>
+            </div>
+            <div class="layericon layericon-2 pull-right clickable" v-on:click="viewData(layer)">
+                <span class="oi oi-grid-three-up"></span>
+			</div>
+
+			<span v-for="(taxon, index) in layer.taxa">
+				{{ taxon.scientificName }} <span class="smaller">{{ taxon.scientificNameAuthorship }}</span><span v-if="index < layer.taxa.length - 1"><br/></span>
+			</span>
+
+			<span v-if="layer.startyear != null || layer.endyear != null" class="count">
+				<br/>{{ layer.startyear }} - {{ layer.endyear }}
+			</span>
+			<span v-if="layer.geometry != null" class="smaller">
+				<br/>polygon filter
+			</span>
+			<br/><span class="smaller">{{ layer.count }} records</span>
+			<br/><div>
+			<span v-if="layer.colors.length > 1" class="colorsquare" :style="{ 'background-color': color }" v-for="color in layer.colors"></span>
+			<span v-if="layer.colors.length == 1" class="colorsquarelong" :style="{ 'background-color': layer.colors[0] }"></span>
 			</div>
 		</div>
+
 	</div>
 </template>
 
