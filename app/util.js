1712f695b6737d4fa166c35ec21c703a763b52c5
Pieter Provoost <pieterprovoost@gmail.com>
2018-01-24 12:01:42 +0100

    refactoring

--- app/util.js
+++ app/util.js
@@ -24,6 +24,61 @@ const createQuery = function(criteria) {
 	return q
 }
 
+const makeScales = function() {
+	return {
+		red: {
+			colors: ["#ffffcc", "#ffeda0", "#fed976", "#feb24c", "#fd8d3c", "#fc4e2a", "#e31a1c", "#b10026"]
+		},
+		blue: {
+			colors: ["#ffffd9", "#edf8b1", "#c7e9b4", "#7fcdbb", "#41b6c4", "#1d91c0", "#225ea8", "#0c2c84"]
+		},
+		green: {
+			colors: ["#ffffe5","#f7fcb9","#d9f0a3","#addd8e","#78c679","#41ab5d","#238443","#005a32"]
+		},
+		bluemono: {
+			colors: ["#f7fbff","#deebf7","#c6dbef","#9ecae1","#6baed6","#4292c6","#2171b5","#084594"]
+		},
+		pinkmono: {
+			colors: ["#fff7f3","#fde0dd","#fcc5c0","#fa9fb5","#f768a1","#dd3497","#ae017e","#7a0177"]
+		},
+		blueeven: {
+			colors: ["#4292c6","#4292c6","#4292c6","#4292c6","#4292c6","#4292c6","#4292c6","#4292c6"]
+		},
+		redeven: {
+			colors: ["#fc4e2a", "#fc4e2a", "#fc4e2a", "#fc4e2a", "#fc4e2a", "#fc4e2a", "#fc4e2a", "#fc4e2a"]
+		},
+		greeneven: {
+			colors: ["#41ab5d","#41ab5d","#41ab5d","#41ab5d","#41ab5d","#41ab5d","#41ab5d","#41ab5d"]
+		}
+	}
+}
+
+const getColor = function(d, colors) {
+    if (colors.length == 1) {
+        return colors[0]
+    } else {
+        if (d > 1000) {
+            return colors[7]
+        } else if (d > 500) {
+            return colors[6]
+        } else if (d > 200) {
+            return colors[5]
+        } else if (d > 100) {
+            return colors[4]
+        } else if (d > 50) {
+            return colors[3]
+        } else if (d > 20) {
+            return colors[2]
+        } else if (d > 10) {
+            return colors[1]
+        } else {
+            return colors[0]
+        }
+    }
+}
+
 module.exports = {
-	createQuery: createQuery
+	createQuery: createQuery,
+    makeScales: makeScales,
+    getColor: getColor
 }
