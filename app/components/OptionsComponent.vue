33267d183761b6e5732e0daea2afb060168cd627
Pieter Provoost <pieterprovoost@gmail.com>
2018-02-21 16:30:54 +0100

    basemap

--- app/components/OptionsComponent.vue
+++ app/components/OptionsComponent.vue
@@ -10,6 +10,7 @@
                 <br/><input type="radio" name="baselayer" v-model="sharedState.baseLayer" value="https://cartodb-basemaps-{s}.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png" id="positron"><label for="positron" class="radiolabel">CartoDB.PositronNoLabels</label>
                 <br/><input type="radio" name="baselayer" v-model="sharedState.baseLayer" value="https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png" id="dark"><label for="dark" class="radiolabel">CartoDB.DarkMatterNoLabels</label>
                 <br/><input type="radio" name="baselayer" v-model="sharedState.baseLayer" value="http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png" id="osm"><label for="osm" class="radiolabel">OpenStreetMap.BlackAndwhite</label>
+                <br/><input type="radio" name="baselayer" v-model="sharedState.baseLayer" value="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" id="osmmapnik"><label for="osmmapnik" class="radiolabel">OpenStreetMap.Mapnik</label>
                 <br/><input type="radio" name="baselayer" v-model="sharedState.baseLayer" value="https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}" id="esri"><label for="esri" class="radiolabel">Esri.WorldImagery</label>
             </div>
         </div>
